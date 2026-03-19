4cfb919a2235a77a80bd93567d50334685a7eadf
Karthick Bala <karthickb399@gmail.com>
2026-03-19 23:41:04 +0530

    Update bot.py

--- bot.py
+++ bot.py
@@ -8,8 +8,8 @@ import os
 
 TOKEN = os.getenv("TOKEN")
 
-GUILD_ID = 1418109436928458775
-CATEGORY_ID = 1442032064244486204
+GUILD_ID = "1418109436928458775"
+CATEGORY_ID = "1442032064244486204"
 
 intents = discord.Intents.default()
 bot = commands.Bot(command_prefix="!", intents=intents)
@@ -19,6 +19,17 @@ vc = None
 @tasks.loop(minutes=1)
 async def check_time():
     global vc
+
+    guild = bot.get_guild(GUILD_ID)
+    category = guild.get_channel(CATEGORY_ID)
+
+    print("Guild:", guild)
+    print("Category:", category)
+
+    if vc is None:
+        vc = await guild.create_voice_channel("TEST VC", category=category)
+        print("VC Created")
+        
     now = datetime.datetime.now(IST).strftime("%H:%M")
     print("Current time:", now)
     guild = bot.get_guild(GUILD_ID)
@@ -27,7 +38,7 @@ async def check_time():
         
     category = guild.get_channel(CATEGORY_ID)
 
-    if now == "2335" and vc is None:
+    if now == "2343" and vc is None:
         vc = await guild.create_voice_channel("Scheduled VC")
         print("VC Created")
 
